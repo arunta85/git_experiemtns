ffba4ba814528f16ff9871645a66b74d9ea438f8
Arun Thdukutchi Ananthakrishnan <arunta85@gmail.com>
2025-08-17 07:13:03 +0200

    Revert "i dont know"
    This reverts commit 24f6b05cec411efe27f05f7a893ecce4eab52d2b.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,5 @@
-# Usage: python HelloWorld.py  (Edited to demonstrate saving in terminal editors)
 def main():
-    print("Hello, World! Arun here")
+    print("Hello, World! Arun here. My third commit")
 
 if __name__ == "__main__":
     main()

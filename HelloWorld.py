24f6b05cec411efe27f05f7a893ecce4eab52d2b
Arun Thdukutchi Ananthakrishnan <arunta85@gmail.com>
2025-08-17 06:59:01 +0200

    i dont know

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,6 @@
+# Usage: python HelloWorld.py  (Edited to demonstrate saving in terminal editors)
 def main():
-    print("Hello, World! Arun here. My third commit")
+    print("Hello, World! Arun here")
 
 if __name__ == "__main__":
     main()
